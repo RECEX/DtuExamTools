9a804825960d44832e4531aa5f1beba95685c179
Arnav Gupta <android@championswimmer.tk>
2013-05-30 20:41:27 +0530

    Add Copyright

--- src/com/RecEx/dtuexamtools/Splash.java
+++ src/com/RecEx/dtuexamtools/Splash.java
@@ -1,3 +1,24 @@
+/**********************************************************************
+    RecEX DTU Exam  Tools
+    Copyright (C) 2013  RecEx
+
+The source code of this program is shared under the RECEX
+SHARED SOURCE LICENSE (version 1.0).
+
+The source code is shared for referrence and academic purposes
+with the hope that people can read and learn from it. This is not
+Free and Open Source software, and code is not redistributable
+without permission of the author. Read the RECEX SHARED
+SOURCE LICENSE for details 
+
+The source codes does not come with any warranty including
+the implied warranty of merchandise. 
+
+You should have received a copy of the RECEX SHARED SOURCE
+LICENSE in the form of a License.txt file in the root of the source
+directory. If not, please refer to 
+<https://raw.github.com/Recex/Licenses/master/SharedSourceLicense/LICENSE.txt>
+**********************************************************************/
 package com.RecEx.dtuexamtools;
 
 import android.annotation.TargetApi;
